3b21164a0a5d73e5779a4229460ec1e481a9079b
demiz99 <demiz99@mail.ru>
2022-03-15 21:34:29 +0500

    hotfix: из-за visibility: visible - резкое появление происходит. Как-то исправить через transition не получилось

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,14 +1,26 @@
-const acc = document.getElementsByClassName("aside__accordion_slide");
-let i;
+const accordionItems = document.querySelectorAll(".aside__accordion_slide");
+const defaultActiveBlocks = document.querySelectorAll('.aside__accordion_active');
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("aside__accordion_active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {  
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+const openPanelBlock = (panel) => {
+  if (panel.style.maxHeight){
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = `${panel.scrollHeight}px`;
+  }
+  panel.classList.toggle('aside__panel_visible')
+};
+
+const handleAccordionClick = (event) => {
+  const panel = event.target.nextElementSibling;
+  event.target.classList.toggle("aside__accordion_active");
+  openPanelBlock(panel);
 }
+
+accordionItems.forEach((item) => {
+  item.addEventListener('click', handleAccordionClick)
+});
+
+defaultActiveBlocks.forEach((item) => {
+  const panel = item.nextElementSibling;
+  openPanelBlock(panel)
+});
